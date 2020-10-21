c5fc7c065061ef5c0d29a3b2f120a200aa536d8b
Murat Furuncu <murat.furuncu@gmail.com>
2020-10-21 21:20:35 +0200

    trying push up

---
+++ src/test/java/com/add_file_here/EU3_Practice.java
@@ -0,0 +1,8 @@
+package com.add_file_here;
+
+public class EU3_Practice {
+
+    public static void main(String[] args) {
+        System.out.println("EU# is openning pull murat");
+    }
+}
---
+++ src/test/java/com/add_file_here/SampleFile.java
@@ -0,0 +1,9 @@
+package com.add_file_here;
+
+public class SampleFile {
+
+    public  static  void  main(String[] args) {
+
+        System.out.println("add some stuff");
+    }
+}
